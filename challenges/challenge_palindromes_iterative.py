41a9f7673a16ee3dbb408834a317f72af7093932
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-16 18:56:35 -0300

    Feat: Functionality of the is_palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    length = len(word)
+    mid = length // 2
+    for i in range(mid):
+        if word[i] != word[length - i - 1]:
+            return False
+    return True if length > 0 else False
