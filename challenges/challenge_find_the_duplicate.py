0d22ea69486e43af9e0cb63b1dd2fce5e44d8568
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-16 17:36:42 -0300

    Feat: Functionality of the find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,9 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums: return False
+    nums.sort()
+    for index, num in enumerate(nums[1:], start = 1): 
+        if not isinstance(num, int) or num < 0:
+            return False
+        if nums[index - 1] == nums[index]:
+            return num
+    return False
